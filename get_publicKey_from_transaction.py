a50d0d429a6e6e045439cbbb76fbd4136ccbdb46
tokimay <tokimay@gmail.com>
2025-02-25 12:32:50 +0330

    License 403.12

--- get_publicKey_from_transaction.py
+++ get_publicKey_from_transaction.py
@@ -1,3 +1,22 @@
+
+# This file is part of https://github.com/tokimay/ethereum_public_Key
+# Copyright (C) 2016 https://github.com/tokimay
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <https://www.gnu.org/licenses/>.
+# This software is licensed under GPLv3. If you use or modify this project,
+# you must include a reference to the original repository: https://github.com/tokimay/ethereum_public_Key
+
 from eth_account._utils.legacy_transactions import serializable_unsigned_transaction_from_dict
 from eth_account._utils.signing import to_standard_v, extract_chain_id
 import web3
